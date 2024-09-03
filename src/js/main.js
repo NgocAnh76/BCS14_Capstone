a8b76f3b7b20d904bb91447375eac3eecb3dfa8e
duong-nguyenngoc <nguyenngocduong265@gmail.com>
2024-09-03 16:56:36 +0700

    update fslightbox

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,1 @@
-const button = document.getElementById("myButton");
 
-// Add click event to the button
-button.addEventListener("click", function (event) {
-  // Prevent the event from bubbling up to the document
-  event.stopPropagation();
-  // Toggle 'active' class to rotate the icon
-  button.classList.toggle("active");
-});
-
-// Remove the active class when clicking outside the button
-document.addEventListener("click", function () {
-  button.classList.remove("active");
-});
